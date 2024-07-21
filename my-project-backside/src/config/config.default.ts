98b9e0555404068acb38100090e91a998c597822
dong-frank <dongshen_rao@smial.nju.edu.cn>
2024-07-21 14:04:02 +0800

    后端代码库初始化和构建

---
+++ my-project-backside/src/config/config.default.ts
@@ -0,0 +1,9 @@
+import { MidwayConfig } from '@midwayjs/core';
+
+export default {
+  // use for cookie sign key, should change to your own and keep security
+  keys: '1721541783879_7125',
+  koa: {
+    port: 7001,
+  },
+} as MidwayConfig;
